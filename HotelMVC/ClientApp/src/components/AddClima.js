d24aa8d09e1a79939cb80d1d9c3f1d47acc43049
Alisson Candaten de Oliveira <alicandaten23@gmail.com>
2021-10-18 11:56:19 -0300

    Adicionado a tela de Cadastro de clima + Pacote do react para forms avançados.

---
+++ HotelMVC/ClientApp/src/components/AddClima.js
@@ -0,0 +1,70 @@
+﻿import React, { Component} from "react";
+import { Form, Field } from 'react-advanced-form'
+import { Input, Button, Select } from 'react-advanced-form-addons'
+
+
+export  class AddClima extends Component {
+
+  registerUser = ({ serialized, fields, form }) => {
+    return fetch('https://backend.dev', {
+      body: JSON.stringify(serialized)
+    })
+  }
+
+
+
+  render() {
+    return (
+      <Form
+        action={this.registerUser}>
+        <Field.Group name="primaryInfo">
+          <Input
+            name="userEmail"
+            type="email"
+            label="E-mail"
+            required />
+        </Field.Group>
+
+        <Input
+          name="userPassword"
+          type="password"
+          label="Password"
+          required />
+        <Input
+          name="confirmPassword"
+          type="password"
+          label="Confirm password"
+          required
+          skip />
+
+        <Select>
+          <option value="admin">Administrator</option>
+          <option value="editor">Editor</option>
+        </Select>
+
+        <Field.Group name="primaryInfo">
+          <Input
+            name="firstName"
+            label="First name"
+            required={({ get }) => {
+              return !!get(['primaryInfo', 'lastName', 'value'])
+            }} />
+          <Input
+            name="lastName"
+            label="Last name"
+            required={({ get }) => {
+              return !!get(['primaryInfo', 'firstName', 'value'])
+            }} />
+        </Field.Group>
+
+
+
+        <Button primary>Register</Button>
+      </Form>
+    );
+  }
+
+
+
+
+}
